8940e07cd73c829a0b1a01485eb7bd21e9175250
BansiTilva <bansitilva8@gmail.com>
2022-09-17 16:22:25 +0530

    Updates

---
+++ precache-manifest.300e9c7741c14e59c23b1d83ad5ac8f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3393e02a5a5f679092781184d71f30e",
+    "url": "/airbnb-clone-20DCS136/index.html"
+  },
+  {
+    "revision": "5204efd19cd4bb25612c",
+    "url": "/airbnb-clone-20DCS136/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "7a39c9b1611a78416b12",
+    "url": "/airbnb-clone-20DCS136/static/css/main.acea540d.chunk.css"
+  },
+  {
+    "revision": "5204efd19cd4bb25612c",
+    "url": "/airbnb-clone-20DCS136/static/js/2.29c5b276.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/airbnb-clone-20DCS136/static/js/2.29c5b276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a39c9b1611a78416b12",
+    "url": "/airbnb-clone-20DCS136/static/js/main.ba0ce174.chunk.js"
+  },
+  {
+    "revision": "4a9c4075db9554b2f8b5",
+    "url": "/airbnb-clone-20DCS136/static/js/runtime-main.7dcbe1d2.js"
+  }
+]);
